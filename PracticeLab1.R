f5eb75138378e535f3fbc0edeecfd7d5ef928369
noakrawczyk <31893177+noakrawczyk@users.noreply.github.com>
2017-09-13 00:24:10 -0400

    added more commands

--- PracticeLab1.R
+++ PracticeLab1.R
@@ -1,3 +1,16 @@
 practice_lab1
 
-**make changes?
+##change date
+as.Date(ufo_data$datetime)
+
+##capitalize (not sure why this didn't work)
+toupper(ufo_data$state)
+
+##latitude to numeric (not sure why this is only turning to factor)
+class(ufo_data$latitude) # "numeric"
+
+##longitude to numeric
+class(ufo_data$longitude) # "numeric"
+
+##write csv file
+write.csv(ufo_data, file = "new_ufo_data.csv")
